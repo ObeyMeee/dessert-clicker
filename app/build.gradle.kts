b308e2dd53f1aad4facea0d1d151fad896f193db
Andromeda <geraskin19970309@gmail.com>
2023-10-16 10:40:23 +0300

    starter code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ua.com.andromeda.dessertclicker"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ua.com.andromeda.dessertclicker"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
